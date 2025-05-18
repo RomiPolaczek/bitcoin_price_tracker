2f81bcdb42d36db247ebc69a490f8550f5b31fbb
romipolaczek <168196934+romipolaczek@users.noreply.github.com>
2025-05-18 16:50:35 +0300

    add console logger

--- logger_setup.py
+++ logger_setup.py
@@ -1,11 +1,17 @@
 import logging
+import sys
+
 from config import LOG_FILE_NAME
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 file_handler = logging.FileHandler(LOG_FILE_NAME)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
-
 logger.addHandler(file_handler)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
